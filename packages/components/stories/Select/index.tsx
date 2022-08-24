6dcb7aa98dcc3bec422814e52808e67df55f8ca9
taotao68 <GL11130306@163.com>
2022-08-24 17:28:07 +0800

    fix-fix:some

--- packages/components/stories/Select/index.tsx
+++ packages/components/stories/Select/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react'
+import React,{ Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
